20aa57ae5981ae4ba7526aeeecb381176295fad8
Yaroslav Borys <borrline@gmail.com>
2024-11-01 19:28:39 +0300

    last changes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,18 @@
-import { Tabs } from "expo-router";
+import { Stack, Tabs } from "expo-router";
 import React from "react";
 
 import { TabBarIcon } from "@components/navigation/TabBarIcon";
 import { Colors } from "@constants/Colors";
 import { useColorScheme } from "@hooks/useColorScheme";
+import Login from "./login";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const isLoggedIn = true;
 
   return (
     <Tabs
+      initialRouteName="index"
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
