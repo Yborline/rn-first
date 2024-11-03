1b24d6b4d4d7b81e875f5e1361e57cc422aee3e9
Yaroslav Borys <borrline@gmail.com>
2024-11-03 22:19:34 +0300

    added page puplications

---
+++ app/(tabs)/publications.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Publications = () => {
+  return (
+    <View>
+      <Text>Publications</Text>
+    </View>
+  );
+};
+
+export default Publications;
