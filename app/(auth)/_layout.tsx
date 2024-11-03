6fecdb26acc1bdb6deedc222fcaae27841ab17e0
Yaroslav Borys <borrline@gmail.com>
2024-11-03 22:15:22 +0300

    added new layout

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Authlayaut = () => {
+  return (
+    <View>
+      <Text>_layaut</Text>
+    </View>
+  );
+};
+
+export default Authlayaut;
