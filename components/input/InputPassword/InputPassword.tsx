f2b80606523d0f6b3c2fead36ecf0210b5e14c85
Yaroslav Borys <borrline@gmail.com>
2024-11-03 22:18:02 +0300

    fixed styles input password , and added some files

--- components/input/InputPassword/InputPassword.tsx
+++ components/input/InputPassword/InputPassword.tsx
@@ -1,18 +1,13 @@
 import { FC, useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  Touchable,
-  View,
-} from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 interface IInputPass {
   placeholder: string;
   onChange: (text: string) => void;
 }
+import { styles } from "../input.styles";
 
 const InputPassword: FC<IInputPass> = ({ placeholder, onChange }) => {
+  const [isFocused, setIsFocused] = useState(false);
   const [visiblePass, setVisiblePass] = useState<boolean>(true);
   const handleChangeVisiblePass = () => {
     setVisiblePass((prev) => !prev);
@@ -21,14 +16,19 @@ const InputPassword: FC<IInputPass> = ({ placeholder, onChange }) => {
   return (
     <View style={styles.conteiner}>
       <TextInput
-        style={styles.textInput}
+        style={[styles.textInput, isFocused && styles.inputFocused]}
         placeholder={placeholder}
         onChangeText={onChange}
         secureTextEntry={visiblePass}
         textContentType="password"
         autoComplete="off"
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
       />
-      <TouchableOpacity onPress={handleChangeVisiblePass}>
+      <TouchableOpacity
+        style={styles.textPass}
+        onPress={handleChangeVisiblePass}
+      >
         <Text>{visiblePass ? "Показати" : "Приховати"}</Text>
       </TouchableOpacity>
     </View>
@@ -37,20 +37,20 @@ const InputPassword: FC<IInputPass> = ({ placeholder, onChange }) => {
 
 export default InputPassword;
 
-const styles = StyleSheet.create({
-  conteiner: {
-    flexDirection: "row",
-    alignItems: "center",
-    borderWidth: 1,
-    borderColor: "gray",
-    borderRadius: 10,
-    paddingHorizontal: 10,
-    marginBottom: 10,
-  },
-  textInput: {
-    flex: 1,
-    height: 40,
-    padding: 10,
-    fontSize: 16,
-  },
-});
+// const styles = StyleSheet.create({
+//   conteiner: {
+//     flexDirection: "row",
+//     alignItems: "center",
+//     borderWidth: 1,
+//     borderColor: "gray",
+//     borderRadius: 10,
+//     paddingHorizontal: 10,
+//     marginBottom: 10,
+//   },
+//   textInput: {
+//     flex: 1,
+//     height: 40,
+//     padding: 10,
+//     fontSize: 16,
+//   },
+// });
