d5e25c4aef40dcbab5e4db315e4a68c224fc0e85
Yaroslav Borys <borrline@gmail.com>
2024-11-03 22:56:10 +0300

    changed tabs (added new icons ) and try new logic

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,26 @@ import React from "react";
 import { TabBarIcon } from "@components/navigation/TabBarIcon";
 import { Colors } from "@constants/Colors";
 import { useColorScheme } from "@hooks/useColorScheme";
+import Feather from "@expo/vector-icons/Feather";
 import Login from "./login";
+import { Image, Text, View } from "react-native";
+import saveIcon from "../../assets/images/icons/bookmark.png";
+
+// const TabIcon = ({ icon, color, name, focused }) => {
+//   return (
+//     <View>
+//       <Image
+//         source={icon}
+//         resizeMode="contain"
+//         tintColor={color}
+//         className="w-6 h-6"
+//       />
+//       <Text className={`${focused ? "font-extrabold" : "font-normal"}`}>
+//         {name}
+//       </Text>
+//     </View>
+//   );
+// };
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -12,13 +31,13 @@ export default function TabLayout() {
 
   return (
     <Tabs
-      initialRouteName="index"
+      initialRouteName="login"
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
       }}
     >
-      <Tabs.Screen
+      {/* <Tabs.Screen
         name="index"
         options={{
           title: "Home",
@@ -29,16 +48,40 @@ export default function TabLayout() {
             />
           ),
         }}
+      /> */}
+      <Tabs.Screen
+        name="publications"
+        options={{
+          title: "Публікації",
+          headerShown: true,
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? "apps" : "apps"} color={color} />
+          ),
+        }}
       />
       <Tabs.Screen
-        name="explore"
+        name="create"
         options={{
-          title: "Explore",
+          title: "Створити",
+          headerShown: true,
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "code-slash" : "code-slash-outline"}
-              color={color}
-            />
+            // <TabIcon
+            //   icon={saveIcon}
+            //   color={"red"}
+            //   name="Публікації"
+            //   focused={focused}
+            // />
+            <TabBarIcon name={focused ? "add" : "add"} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Профіль",
+          headerShown: true,
+          tabBarIcon: ({ color, focused }) => (
+            <Feather name={focused ? "user" : "user"} size={24} color={color} />
           ),
         }}
       />
