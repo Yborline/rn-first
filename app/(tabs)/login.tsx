b4ad89202f8aae20a0feb5d75e3a699629a29d62
Yaroslav Borys <borrline@gmail.com>
2024-11-14 20:15:50 +0300

    update login page

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,22 +1,19 @@
 import {
   Keyboard,
   KeyboardAvoidingView,
-  Platform,
-  StyleSheet,
   TouchableWithoutFeedback,
   Text,
   View,
-  Button,
   Alert,
   ImageBackground,
   TouchableOpacity,
 } from "react-native";
 import startBg from "@assets/images/background/startBg.png";
 import { useState } from "react";
-import { TextInput } from "react-native";
 import { Link } from "expo-router";
 import Input from "@components/input/Input";
 import InputPassword from "@components/input/InputPassword/InputPassword";
+import { styles } from "../styles/login";
 const Login = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
@@ -36,9 +33,7 @@ const Login = () => {
           >
             <View style={styles.boxForm}>
               <View style={styles.boxInput}>
-                <Text className="text-3xl font-bold" style={styles.textTitle}>
-                  Увійти
-                </Text>
+                <Text style={styles.textTitle}>Увійти</Text>
                 <Input
                   type="emailAddress"
                   placeholder="Ваша електронна адреса"
@@ -61,73 +56,3 @@ const Login = () => {
 };
 
 export default Login;
-
-const styles = StyleSheet.create({
-  conteiner: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    fontFamily: "DMSerifItal",
-  },
-  image: {
-    flex: 1,
-  },
-  textTitle: {
-    fontSize: 30,
-    lineHeight: 40,
-    fontFamily: "Rubick",
-    textAlign: `center`,
-    paddingBottom: 33,
-    // fontFamily: "DMSerifItal",
-  },
-  textInput: {
-    margin: 0,
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    padding: 10,
-    width: "100%",
-    borderRadius: 10,
-    marginBottom: 10,
-  },
-  boxForm: {
-    height: 375,
-    width: `100%`,
-    backgroundColor: `white`,
-    paddingVertical: 32,
-    paddingHorizontal: 16,
-    paddingBottom: 45,
-    paddingLeft: 16,
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
-  },
-  changeMargin: {
-    marginBottom: Platform.OS === "ios" ? 60 : 30,
-  },
-  boxAboiding: {
-    width: `100%`,
-  },
-  buttonAuth: {
-    backgroundColor: `#FF6C00`,
-    borderRadius: 100,
-
-    height: 51,
-    display: `flex`,
-    justifyContent: "center",
-    alignItems: `center`,
-  },
-  buttonText: {
-    color: `white`,
-  },
-  linkText: {
-    textAlign: `center`,
-    marginTop: 16,
-    color: `#1B4371`,
-  },
-  boxInput: {
-    marginBottom: 43,
-  },
-});
