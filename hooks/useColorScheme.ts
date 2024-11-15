955798226a886cfd1ad3874657b61d3bc7340931
Yaroslav Borys <borrline@gmail.com>
2024-11-15 21:17:25 +0300

    changed hook useColorSheme

--- hooks/useColorScheme.ts
+++ hooks/useColorScheme.ts
@@ -1,1 +1,5 @@
-export { useColorScheme } from 'react-native';
+export { useColorScheme } from "react-native";
+import { useTheme as useNavigationTheme } from "@react-navigation/native";
+import { CustomTheme } from "interfaces/theme";
+
+export const useCustomTheme = () => useNavigationTheme() as CustomTheme;
