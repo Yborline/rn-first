57b420f3265787b90a13a5c1406d484ba8d15557
Yaroslav Borys <borrline@gmail.com>
2024-11-15 21:16:53 +0300

    small changed Layout auth

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,11 +1,15 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Stack } from "expo-router";
 
 const Authlayaut = () => {
   return (
-    <View>
-      <Text>_layaut</Text>
-    </View>
+    <>
+      <Stack>
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="register" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 };
 
