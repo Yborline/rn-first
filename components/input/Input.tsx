28bf4b402cb2f3eacbaf740796c6f569d973efa0
Yaroslav Borys <borrline@gmail.com>
2024-11-15 21:09:38 +0300

    changed component Input (united inputPassword and input)

--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -1,26 +1,60 @@
 import { FC, useState } from "react";
-import { StyleSheet, TextInput } from "react-native";
-import { styles } from "./input.styles";
-interface IInput {
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+interface IInputPass {
   placeholder: string;
   onChange: (text: string) => void;
-  type: "username" | "emailAddress" | "none" | "nickname";
+  type?: string;
 }
+import { styles } from "./input.styles";
 
-const Input: FC<IInput> = ({ placeholder, onChange, type }) => {
+const InputPassword: FC<IInputPass> = ({ placeholder, onChange }) => {
   const [isFocused, setIsFocused] = useState(false);
+  const [visiblePass, setVisiblePass] = useState<boolean>(true);
+  const handleChangeVisiblePass = () => {
+    setVisiblePass((prev) => !prev);
+  };
 
   return (
-    <TextInput
-      style={[styles.textInput, isFocused && styles.inputFocused]}
-      placeholder={placeholder}
-      placeholderTextColor="rgba(189, 189, 189, 1)"
-      onChangeText={onChange}
-      textContentType={type}
-      onFocus={() => setIsFocused(true)}
-      onBlur={() => setIsFocused(false)}
-    />
+    <View style={styles.conteiner}>
+      <TextInput
+        style={[styles.textInput, isFocused && styles.inputFocused]}
+        placeholder={placeholder}
+        placeholderTextColor="rgba(189, 189, 189, 1)"
+        onChangeText={onChange}
+        secureTextEntry={placeholder === "Пароль" && visiblePass}
+        // textContentType="password"
+
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+      {placeholder === "Пароль" && (
+        <TouchableOpacity
+          style={styles.textPass}
+          onPress={handleChangeVisiblePass}
+        >
+          <Text>{visiblePass ? "Показати" : "Приховати"}</Text>
+        </TouchableOpacity>
+      )}
+    </View>
   );
 };
 
-export default Input;
+export default InputPassword;
+
+// const styles = StyleSheet.create({
+//   conteiner: {
+//     flexDirection: "row",
+//     alignItems: "center",
+//     borderWidth: 1,
+//     borderColor: "gray",
+//     borderRadius: 10,
+//     paddingHorizontal: 10,
+//     marginBottom: 10,
+//   },
+//   textInput: {
+//     flex: 1,
+//     height: 40,
+//     padding: 10,
+//     fontSize: 16,
+//   },
+// });
