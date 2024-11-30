7d2a7b4e61e6604336ecf318c99162df05dc0b83
Yaroslav Borys <borrline@gmail.com>
2024-11-30 15:37:08 +0300

    added interface User

---
+++ interfaces/user.tsx
@@ -0,0 +1,12 @@
+export interface User {
+  $collectionId: string;
+  $createdAt: string;
+  $databaseId: string;
+  $id: string;
+  $permissions: string[];
+  $updatedAt: string;
+  accountId: string;
+  avatar: string;
+  email: string;
+  username: string;
+}
